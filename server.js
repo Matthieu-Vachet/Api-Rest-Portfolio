bd927faf32e24c91726106015ba5fafd48476b59
Matthieu-V <79032023+Matthieu-Vachet@users.noreply.github.com>
2024-02-17 03:13:05 +0100

    Ajouter le module CORS et configurer les en-têtes CORS

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const projetRoutes = require("./routes/projetsRoutes");
 const experienceRoutes = require("./routes/experienceRoutes");
+const cors = require("cors");
 let expressSwagger = require("express-swagger-generator")(app);
 
-app.use(express.json());
+app.use(cors());
 
 // Ajout des en-têtes CORS
 app.use((req, res, next) => {
